db44faf8e281c88a9f7f719a785d79851f0ef923
Qu1dy <71177869+Qu1dy@users.noreply.github.com>
2025-06-30 01:44:17 +0300

    feat: register number buttons

--- index.js
+++ index.js
@@ -2,3 +2,16 @@ const addButton = document.querySelector("#add");
 const substractButton = document.querySelector("#substract");
 const divideButton = document.querySelector("#divide");
 const multiplyButton = document.querySelector("#multiply");
+const numbers = document.querySelectorAll(".number");
+
+const registerNumbers = () => {
+    numbers.forEach(number => {
+        number.addEventListener("click", manage);
+    })
+}
+
+const manage = () => {
+    console.log("called");
+}
+
+registerNumbers();
