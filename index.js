2e190c6f23864073c3ebd2853ecb4a56173a44d7
Qu1dy <71177869+Qu1dy@users.noreply.github.com>
2025-06-30 01:37:17 +0300

    feat: get operator buttons in js

---
+++ index.js
@@ -0,0 +1,4 @@
+const addButton = document.querySelector("#add");
+const substractButton = document.querySelector("#substract");
+const divideButton = document.querySelector("#divide");
+const multiplyButton = document.querySelector("#multiply");
